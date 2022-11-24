40233f2e03e56f06304d14ac11b5c67664f4731f
Adam <adaand00@gmail.com>
2022-11-24 22:47:41 +0100

    Encoder and i2s compiling

--- SSQB-Code/src/main.cpp
+++ SSQB-Code/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <NeoPixelBus.h>
 #include <ESP32Encoder.h>
+#include "driver/i2s.h"
+#include "driver/gpio.h"
 
 // Pins
 #define POT1 1
@@ -31,6 +33,33 @@ NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(pixelCount, PIXELPIN);
 // Encoder
 ESP32Encoder encoder;
 
+void install_i2s(){
+  // I2S
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX), // Both TX and RX in master mode
+    .sample_rate = 48000,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_24BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // Interrupt level 1, default 0
+    .dma_buf_count = 2,
+    .dma_buf_len = 128,
+    .use_apll = true,
+    .mclk_multiple = I2S_MCLK_MULTIPLE_256
+  };
+
+  i2s_pin_config_t pin_config = {
+    .mck_io_num = 0,
+    .bck_io_num = BCLK,
+    .ws_io_num = LRCLK,
+    .data_out_num = DOUT,
+    .data_in_num = DIN
+  };
+
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,13 +73,16 @@ void setup() {
   encoder.attachHalfQuad(ENC_A, ENC_B);
   encoder.clearCount();
 
+  // Initialize I2S
+  install_i2s();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // set whole strip to red
   int h = encoder.getCount();
-  strip.ClearTo(HslColor(h/100.0, 0, 1));
+  strip.ClearTo(HslColor(h/100.0, 1, 0.5));
   strip.Show();
   delay(10);
 }
